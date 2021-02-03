718254573b12b5be85191daea6cf8e21bb0525fe
Ahmed Bishree <ahmedbishree@gmail.com>
2021-02-03 14:42:48 +0300

    third commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     println!("Hello, world!");
     println! ("ahmed this is goooood");
-    println!("ahmed is doing git")
+    println!("ahmed is doing git");
+    println!("test for the git");
 }
 
