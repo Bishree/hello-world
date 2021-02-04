307375fdb5f23e6dd39c8c8c58f49ba3d6ae7cdb
bishree <ahmedbishree@gmail.com>
2021-02-04 22:03:19 +0300

    - functions_01

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+    other_function (5, 6);
+}
+fn other_function(x:i32, y:i32){
+    println!("the value of x is: {} ", x);
+    println!("the value of y is: {} ", y);
+
 }
