84fb6c01cf5ee7e490537e39bdf17269a49c8d9d
Ahmed Bishree <ahmedbishree@gmail.com>
2021-02-05 11:13:41 +0300

    - The loop example

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter *2;
+        }
+    };
 
-    another_function(5);
-
+    println!(" the value of result is {}", result)
  }
-fn another_function (x:i32){
-    println!("The Value of x is: {} ", x)
-
-}
-
