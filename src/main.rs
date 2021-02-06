4811d24beaaf101368a7fa3f4c22cf45020d8dbe
bishree <ahmedbishree@gmail.com>
2021-02-06 20:45:06 +0300

    - first commit

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,45 @@
 use std::io;
 use std::iter::Enumerate;
+use std::convert::AsRef;
 
 fn main() {
     /// TODO
     /// read the input Fahrenheit & celsius from input
-    /// for celsius press C and for Fahrenheit press F
+
     loop {
         println!("for celsius press C and for Fahrenheit press F");
         let mut degree_type = String::new();
+        io::stdin()
+            .read_line(&mut degree_type).unwrap();
+        /// defreciate the input for the required tempreture degree type
+        match degree_type.as_str() {
+            "ahmed" => {
+                println!("Enter the Celsius degree plz:/n");
+                let mut degree_int = String::new();
+                io::stdin().read_line(&mut degree_int).expect("you need to enter an integer");
+                let degree_int_number =degree_int.trim().parse().unwrap();
+                f_conv(degree_int_number);
+                }
+            "ahmed2" => {
+                println!("Enter the Fahrenheit degree plz:/n");
+                let mut degree_int = String::new();
+                io::stdin().read_line(&mut degree_int).expect("you need to enter an integer");
+                let degree_int_number =degree_int.trim().parse().unwrap();
+                c_conv(degree_int_number);
+                }
 
-        io::stdin().read_line(&mut degree_type).expect("something wroooong!");
-        // println!("degree type is: {}", degree_type);
-        // let = match degree_type.trim().parse() {
-        // //     Ok(num)=> num,
-        // //     Err(_) => continue,
-        // // };
-        let mut int_degree_type = degree_type.trim().parse().unwrap();
-        f_conv(int_degree_type);
-        println!("The celsius value is: {} ", degree_type);
-        }
+            _ => {}
+        }println!("Nothing found");
+        // f_conv(int_degree_type);
+        // println!("The celsius value is: {} ", degree_type);
+    }
+        // let mut int_degree_type = degree_type.trim().parse().unwrap();
+
 }
 
 
     // create a function for the convert formula
 fn c_conv (f:u32){
-
         let c = (f-32) * (5 / 9);
         println!("The value of the Celsius is: {}", c);
     }
